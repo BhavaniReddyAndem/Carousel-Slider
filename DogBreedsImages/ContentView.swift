2464d6d8d3d32753e091ee3a203da0f005d8614b
BhavaniReddyAndem <bhavanireddyandem@gmail.com>
2023-05-02 11:36:20 -0500

    Initial Commit

---
+++ DogBreedsImages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DogBreedsImages
+//
+//  Created by Bhavani Reddy Navure on 5/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DogBreedsImages/DogBreedsImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedsImagesApp.swift
+//  DogBreedsImages
+//
+//  Created by Bhavani Reddy Navure on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedsImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
